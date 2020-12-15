be7076cecac0e4c12e8e5ed737152592c01bb154
abner.chamorro <abner.chamorro@peiky.com>
2020-12-15 15:22:20 -0500

    many

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model 
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
 
 # Create your models heroe
 class Product(models.Model):
