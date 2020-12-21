11b068b7f7144ac357c0fccad6e9a7bfbbeb7705
abner.chamorro <abner.chamorro@peiky.com>
2020-12-21 14:40:33 -0500

    change database

--- menudigitalqr/settings.py
+++ menudigitalqr/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'product.apps.ProductConfig',
     'rest_framework',
     'corsheaders',
+    'product'
 ]
 
 MIDDLEWARE = [
